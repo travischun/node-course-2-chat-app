7a04dfdc74fca08cb5c83d45e35dbcd4e070ffbd
Travis Chun <tchun@us.ibm.com>
2017-09-18 09:57:18 -0500

    Init chat app server

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+/*
+console.log(__dirname + '/../public');
+console.log(publicPath);*/
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port,()=>{
+	console.log(`Server up on port: ${port}`);
+});

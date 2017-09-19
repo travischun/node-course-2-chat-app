70f9e2bef2107966400886b7f24644df8c890a0c
Travis Chun <tchun@us.ibm.com>
2017-09-19 10:42:00 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -38,14 +38,29 @@ io.on('connection',(socket)=>{
 			text:"bankai",
 			createdAt: 123
 		});*/
-	
+	socket.emit('newMessage',{
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createdAt: new Date().getTime()
+	});
+	socket.broadcast.emit('newMessage',{
+		from: 'Admin',
+		text: 'New user joined the chat',
+		createdAt: new Date().getTime()
+	});
+
 	socket.on('createMessage',(message)=>{
 		console.log('createMessage', message);
 		io.emit('newMessage',{
 			from: message.from,
 			text: message.text,
 			createdAt: new Date().getTime()
-		})
+		});
+		/*socket.broadcast.emit('newMessage',{
+			from:message.from,
+			text:message.text,
+			createdAt:new Date().getTime()
+		});*/
 	});
 });
 

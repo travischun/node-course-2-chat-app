40985dbaf2d457d6cdbff983088e736c0f9c2864
Travis Chun <tchun@us.ibm.com>
2017-09-20 10:14:35 -0500

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 var socket = io();
 
+
+function scrollToBottom(){
+	//selectors
+	var messages = $('#messages');
+	var newMessage = messages.children('li:last-child');
+	//heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+	}
+}
+
 socket.on('connect',function(){
 	console.log("connected to server");
 
@@ -33,6 +51,7 @@ socket.on('newMessage',function(message){
 	});
 
 	$('#messages').append(html);
+	scrollToBottom();
 	/*
 	console.log("New Message: ",message);
 	var li = $('<li></li>');
@@ -52,6 +71,7 @@ socket.on('newLocationMessage',function(message){
 	});
 
 	$('#messages').append(html);
+	scrollToBottom();
 	/*var li = $('<li></li>');
 	var a = $('<a target="_blank">My current location</a>');
 	li.text(`${formattedTime} ${message.from}: `);
